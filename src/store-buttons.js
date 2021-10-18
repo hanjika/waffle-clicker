f2f09ae4002457929a6df3e7da5cc2aad63d60c4
Hanna Masanjika <hanna.masanjika@gmail.com>
2021-10-18 16:22:19 +0200

    Waffle now image instead of a button, purchased items revealed in city

---
+++ src/store-buttons.js
@@ -0,0 +1,31 @@
+const storeButtons = document.querySelectorAll('.store-button');
+
+for (const storeBtn of storeButtons) {
+    storeBtn.addEventListener('click', purchaseItem);
+}
+
+function purchaseItem(e) {
+    var item = e.target.id;
+    addItemtoCity(item);
+}
+
+function addItemtoCity(item) {
+    if (item === 'cursor') {
+        const newImg = document.createElement('img');
+        newImg.src = 'images/cursor.png';
+        console.log(newImg)
+        document.querySelector('.purchased-cursors').appendChild(newImg);
+    } else {
+        const city = document.querySelector('.purchased');
+        const cityDiv = city.querySelector('.' + item);
+
+        const newImg = document.createElement('img');
+        if (item === 'jean-claude') {
+            newImg.src = 'images/jean-claude.gif';
+        } else {
+            newImg.src = 'images/' + item + '.png';
+        }
+        console.log(newImg)
+        cityDiv.appendChild(newImg);
+    }
+}
